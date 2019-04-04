b4448311267c8641529b90822be379382b2599b1
XuTao <xu.tao@husky.neu.edu>
2019-04-03 23:56:40 -0400

    login finish

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,10 +1,6 @@
 //import { Settings } from 'http2';
 
 export class User {
-    firstName: string;
-    lastName: string;
     userName: string;
-    password: string;
-
-
+    passWord: string;
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -10,6 +10,6 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   create(user: User) {
-    return this.http.post('http//localhost:3000' + '/user/create', user);
+    return this.http.post('http://localhost:3000' + '/user/create', user);
   }
 }
