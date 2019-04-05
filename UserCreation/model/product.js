80b64d44e863010ab519c39a8818a19fcb2a9c38
XuTao <xu.tao@husky.neu.edu>
2019-04-05 16:52:45 -0400

    server

--- UserCreation/model/product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const Product = mongoose.Schema({
-    productName: String,
-    productPrice: Number
-});
-
-module.exports = mongoose.model('Product', Product);
--- UserCreation/route/app.route.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    const user = require('../controller/userController');
-
-    app.post('/user/create',user.create);
-    app.get('/user/:userName/:password',user.getUser);
-    app.delete('/user/:userName',user.delete);
-}
