e81fb14901c963d1cc4986b1c0fb712174a7f460
Huayi Zou <lijiazhy@yeah.net>
2019-04-07 17:51:01 -0400

    term and condition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent} from './signup/signup.component';
 import { UserComponent } from './user/user.component';
+import { TermsComponent } from './terms/terms.component';
   
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'signup', component: SignupComponent},
-  { path: 'user', component: UserComponent}
+  { path: 'user', component: UserComponent},
+  { path: 'terms', component: TermsComponent}
 ];
 
 @NgModule({
